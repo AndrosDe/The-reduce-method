fb5eef5f9480508356ab9942212e7420c363c1c0
AndrosDe <andreas_beier1981@yahoo.de>
2022-04-29 10:13:32 +0000

    initial commit

--- reduce.js
+++ reduce.js
@@ -6,8 +6,56 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr);
+console.log(sum);
+console.log("");
+
+// Expanded version:
+console.log("Expanded version:");
+let sumEx = nums.reduce((acc, curr) => {
+  console.log(
+    "Accoumulator:", acc,
+    "current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr
+});
+console.log(sumEx);
+console.log("");
+
+// Expanded version with initial Value:
+console.log("Expanded version with initial Value (0):");
+let sumIni1 = nums.reduce((acc, curr) => {
+  console.log(
+    "Accoumulator:", acc,
+    "current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr
+}, 0);
+console.log(sumIni1);
+console.log("");
+
+console.log("Expanded version with initial Value (10):");
+let sumIni2 = nums.reduce((acc, curr) => {
+  console.log(
+    "Accoumulator:", acc,
+    "current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr
+}, 10);
+console.log(sumIni2);
+console.log("");
+
+console.log("One-line version with initial Value (0):");
+let sum2 = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum2);
+console.log("");
 
 
+// Complex version:
 const teamMembers = [
   {
     name: 'Andrew',
@@ -32,6 +80,18 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalyrsExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalyrsExperience);
+console.log("");
 
 // Grouping by a property, and totaling it too
+let experianceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+console.log(experianceByProfession);
